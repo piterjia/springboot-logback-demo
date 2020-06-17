b86edbf6e9d769d88f78772d05b251ea76e685ae
贾洪军 <hongjun.jia@ucarinc.com>
2020-06-17 16:15:29 +0800

    init project

---
+++ src/test/java/com/piter/springbootlogbackdemo/SpringBootLogbackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piter.springbootlogbackdemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLogbackDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
